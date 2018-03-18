8d77da99ba5f5c99669511ca922093d10ab31751
yashaswi <31963258+arijithsingh@users.noreply.github.com>
2018-03-18 20:21:31 +0530

    1_sum_5not7.py
    printing the sum of numbers ranging from 1 to 500  which are divisible by 5 but not 7

--- Sets1and2/1_sum_5not7.py
+++ Sets1and2/1_sum_5not7.py
@@ -2,8 +2,10 @@ def sumMultiplesof5not7(n):
 	'''prints the sum of numbers less than n that are multiples of 5 but not 7
 	'''
 	res = 0
-	for i in range(5, n, 5):
-		if(i%7 !=0):
+	i=0
+	for i in range(1,500):
+		if(i%7 !=0 and i%5==0):
 			res+=i
+			
 	print(res)
 sumMultiplesof5not7(500)
